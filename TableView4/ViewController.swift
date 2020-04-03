4ea95c2a66192a9caf30b0e6067a5fceca643690
KazuyaFki <complete501xx@yahoo.co.jp>
2020-04-03 23:57:49 +0900

    Initial Commit

---
+++ TableView4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView4
+//
+//  Created by Kazuya Fukui on 2020/04/03.
+//  Copyright © 2020 Kazuya Fukui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
