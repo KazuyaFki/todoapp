d72dfdf831c9af1664676742f36054102a1d438c
KazuyaFki <complete501xx@yahoo.co.jp>
2020-04-04 10:49:00 +0900

    first commit

--- TableView4/ViewController.swift
+++ TableView4/ViewController.swift
@@ -8,12 +8,76 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource,UITextViewDelegate, UITextFieldDelegate {
+    
+    
+    var todoArray = [String]()
 
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        textField.delegate = self
+       
+        
     }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+        
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return todoArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = todoArray[indexPath.row]
+        cell.selectionStyle = .none
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool
+    {
+        return true
+    }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete {
+            todoArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath as IndexPath], with: UITableView.RowAnimation.automatic)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at:indexPath)
+
+        // チェックマークを入れる
+        cell?.accessoryType = .checkmark
+    }
+
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        todoArray.append(textField.text!)
+        textField.resignFirstResponder()
+        textField.text = ""
+        tableView.reloadData()
+        
+        return true
+    }
+
+
 
 
 }
